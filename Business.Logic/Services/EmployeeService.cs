d880716f7cf6839b1e6ea16ff079688a914c350c
Teodor26 <teodorbear26@gmail.com>
2019-02-01 19:32:43 +0200

    Initital branch

---
+++ Business.Logic/Services/EmployeeService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Business.Logic.Services
+{
+    public interface IEmployeeService
+    {
+        List<BaseEmployee> Getlist();
+    }
+    class EmployeeService
+    {
+    }
+}
---
+++ Employee.BusinessLogic/Services/EmployeeService.cs
@@ -0,0 +1,21 @@
+﻿using Employee.Models.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.BusinessLogic.Services
+{
+    public interface IEmployeeSerice
+    {
+        List<BaseEmployee> GetList();
+    }
+    public class EmployeeService : IEmployeeSerice
+    {
+        public List<BaseEmployee> GetList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
